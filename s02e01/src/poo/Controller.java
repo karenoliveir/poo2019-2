ce58d58812d5b5e5f971ddd9a5a2894eb16c3ff4
Aluno <marcelogoncalvescdd@gmail.com>
2019-08-15 11:34:52 -0300

    s02e01

---
+++ s02e01/src/poo/Controller.java
@@ -0,0 +1,18 @@
+package poo;
+
+public class Controller {
+
+	public static void main(String[] args) {
+		//Questão01
+		/*for (int i=150; i<301; i++) {
+			System.out.println(i);
+			}*/
+		//Questão02
+		double sm = 0;
+		for (int i=1 ; i<1001; i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
